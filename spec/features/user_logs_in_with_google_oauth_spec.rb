8ce13b5f84947eb5b9a34025a0c428766895e5df
Jordan Whitten <jordanwa1947@gmail.com>
2018-12-19 23:58:54 -0700

    adds feature testing for google auth with stub for omniauth

---
+++ spec/features/user_logs_in_with_google_oauth_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'Google Oauth' do
+  it 'takes the user to google to signin and redirects back to dashboard' do
+    stub_omniauth
+
+    visit '/'
+    click_link("Sign in with Google")
+
+    expect(current_path).to eq('/dashboard')
+    expect(page).to have_content("success: You've Successfully Logged In!")
+  end
+
+  it 'allows the user to logout after' do
+    stub_omniauth
+
+    visit '/'
+    click_link("Sign in with Google")
+
+    expect(current_path).to eq('/dashboard')
+
+    click_link 'Logout'
+
+    expect(current_path).to eq('/')
+  end
+end
