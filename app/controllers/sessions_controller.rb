f1c308bb82b322f5255b527689ff6d6302f55abe
Michael Gatewood <michael@mngatewood.com>
2018-12-21 16:47:33 -0700

    Add empty matches session at login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_user(request.env["omniauth.auth"])
     session[:user_id] = @user.id
+    session[:matches] = Hash.new
     flash[:success] = "You've Successfully Logged In!"
     redirect_to dashboard_path
   end
