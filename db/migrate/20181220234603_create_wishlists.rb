c711facbe20d6b099c5d3d79c1c18be312ea1dff
Michael Gatewood <michael@mngatewood.com>
2018-12-21 16:42:47 -0700

    Updates schema to include wishlist with restaurant_id

--- db/migrate/20181220234603_create_wishlists.rb
+++ db/migrate/20181220234603_create_wishlists.rb
@@ -2,7 +2,7 @@ class CreateWishlists < ActiveRecord::Migration[5.2]
   def change
     create_table :wishlists do |t|
       t.references :user, foreign_key: true
-      t.integer :rank, null: false, default: 0
+      t.string :restaurant_id, null: false, default: 0
 
       t.timestamps
     end
