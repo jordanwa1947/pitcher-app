779588e28fd8822b509d747a71279ce529fbd6a9
Michael Gatewood <michael@mngatewood.com>
2018-12-21 16:43:42 -0700

    Add user.local_restaurants method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_one :main_address
+  has_many :wishlists
 
   def self.find_or_create_user(auth)
     where(uid: auth[:uid]).first_or_initialize.tap do |user|
@@ -18,5 +19,22 @@ class User < ApplicationRecord
     only_values = address_attributes.map do |value|
       value[1]
     end.join(' ')
+  end
+
+  def local_restaurants(location)
+    response = conn.get("search?term=restaurant&location=#{location}&radius=1609&limit=20&sort_by=distance")
+    body = JSON.parse(response.body, symbolize_names: true)
+    restaurants = Hash.new
+    body[:businesses].map do |business|
+      restaurants[business[:id]] = 0
+    end
+    return restaurants
+  end
+
+  def conn
+    Faraday.new(:url => "https://api.yelp.com/v3/businesses/") do |faraday|
+      faraday.headers['Authorization'] = "Bearer #{ENV['YELP_API_KEY']}"
+      faraday.adapter  Faraday.default_adapter
+    end
   end
 end
