5581daa6cc45d66b722f19b183f2958688ddc74c
Jordan Whitten <jordanwa1947@gmail.com>
2018-12-23 10:16:34 -0700

    continued test setup for dashboard address info and wishlist

--- app/models/main_address.rb
+++ app/models/main_address.rb
@@ -3,4 +3,10 @@ class MainAddress < ApplicationRecord
   delegate :main_address, to: :user
 
   validates_presence_of :state, :city
+  geocoded_by :build_address
+  after_validation :geocode
+
+  def build_address
+    [address, city, state, zip].compact.join(' ')
+  end
 end
