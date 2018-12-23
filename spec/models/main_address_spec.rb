24f8f19a3439236d08dc167cbaf9746756a291ba
Jordan Whitten <jordanwa1947@gmail.com>
2018-12-23 10:21:04 -0700

    additional model setup after migrations with geocoder setup to automatically find coordinates

--- spec/models/main_address_spec.rb
+++ spec/models/main_address_spec.rb
@@ -5,4 +5,14 @@ describe MainAddress do
     it { should validate_presence_of(:state) }
     it { should validate_presence_of(:city) }
   end
+
+  it 'uses geocoder to lookup coordinates for an address' do
+    VCR.use_cassette('geocode_lookup') do
+      user = create(:user)
+      main_address = create(:main_address, user: user)
+ 
+      expect(main_address.longitude.class).to eq(Float)
+      expect(main_address.longitude.class).to eq(Float)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,16 +4,18 @@ describe 'User' do
   describe 'Instance Methods' do
 
     before(:each) do
-      @user = User.create(email: 'jawesome@gmail.com', 
-                         first_name: 'Jordan',
-                         last_name: 'Awesome', 
-                         uid: '12345')
-      @prime_address = MainAddress.create(address: '17th St LL100',
-                                        state: 'CO',
-                                        city: 'Denver', 
-                                        zip: '80202',
-                                        user_id: @user.id)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      VCR.use_cassette('geocode_lookup', record: :all) do
+        @user = User.create(email: 'jawesome@gmail.com',
+                           first_name: 'Jordan',
+                           last_name: 'Awesome',
+                           uid: '12345')
+        @prime_address = MainAddress.create(address: '17th St LL100',
+                                          state: 'CO',
+                                          city: 'Denver',
+                                          zip: '80202',
+                                          user_id: @user.id)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      end
     end
 
     it '.build address' do
