89bde47f9afe1ee9f803935edbbf0f94d2d3e5da
Jordan Whitten <jordanwa1947@gmail.com>
2018-12-25 18:27:43 -0700

    cant help but try to move some more js out from the directions page to an asset

--- app/assets/javascripts/directions.js
+++ app/assets/javascripts/directions.js
@@ -1,1 +1,13 @@
-
+function calculateRoute(origin, destination, directionsDisplay, directionsService) {
+  directionsService.route({
+    origin: origin,
+    destination: destination,
+    travelMode: 'DRIVING'
+  }, function (response, status) {
+    if(status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
