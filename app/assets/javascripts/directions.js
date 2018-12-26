e2b149c3c71de12c6c9000c88bd70720397b1abc
Jordan Whitten <jordanwa1947@gmail.com>
2018-12-25 18:20:01 -0700

    last refactoring also didnt work, reverting back to the original... almost

--- app/assets/javascripts/directions.js
+++ app/assets/javascripts/directions.js
@@ -1,13 +1,1 @@
-function calculateRoute(origin, destination, directionsDisplay, directionsService) {
-  directionsService.route({
-    origin: origin,
-    destination: destination,
-    travelMode: 'DRIVING'
-  }, function (response, status) {
-    if(status === 'OK') {
-      directionsDisplay.setDirections(response);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
-}
+
