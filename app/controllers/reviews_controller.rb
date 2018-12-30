a910817e51cfa8b5541f53c8da967defb723060c
Katelyn <katelyn.rimkunas@gmail.com>
2018-12-30 13:17:58 -0700

    sad path for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
 
+  before_action :authenticate
+
   def create
     user = User.find(review_params[:user_id])
     Review.create(title: review_params[:title],
