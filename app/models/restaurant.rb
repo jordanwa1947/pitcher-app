58468500dd0a3d6f1b424eba956e4a8f942534fe
Michael Gatewood <michael@mngatewood.com>
2018-12-30 12:17:40 -0700

    Adds additional yelp info to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,12 +1,18 @@
 class Restaurant < ApplicationRecord
   has_many :wishlists
   has_many :reviews
+  has_many :photos
+
+  validates_uniqueness_of :yelp_id
 
   def self.create_self(data)
     create(
       longitude:    data[:coordinates][:longitude],
       latitude:     data[:coordinates][:latitude],
+      yelp_id:      data[:id],
       yelp_link:    data[:url],
+      yelp_rating:  data[:rating],
+      yelp_reviews: data[:review_count],
       phone_number: data[:display_phone],
       address:      data[:location][:display_address],
       name:         data[:name],
--- db/migrate/20181223043040_create_restaurants.rb
+++ db/migrate/20181223043040_create_restaurants.rb
@@ -3,7 +3,10 @@ class CreateRestaurants < ActiveRecord::Migration[5.2]
     create_table :restaurants do |t|
       t.float :longitude
       t.float :latitude
+      t.string :yelp_id
       t.string :yelp_link
+      t.string :yelp_rating
+      t.integer :yelp_reviews
       t.string :phone_number
       t.string :address
       t.string :name
