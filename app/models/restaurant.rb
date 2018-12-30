0bc10cab05f56c8ead9bd5d428c3ce40aacd0905
Katelyn <katelyn.rimkunas@gmail.com>
2018-12-30 13:24:43 -0700

    Restaurant now holds a yelp_id column

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,6 +7,7 @@ class Restaurant < ApplicationRecord
       longitude:    data[:coordinates][:longitude],
       latitude:     data[:coordinates][:latitude],
       yelp_link:    data[:url],
+      yelp_id:      data[:id],
       phone_number: data[:display_phone],
       address:      data[:location][:display_address],
       name:         data[:name],
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_29_215005) do
+ActiveRecord::Schema.define(version: 2018_12_30_202228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_12_29_215005) do
     t.string "name"
     t.string "image"
     t.string "city"
+    t.string "yelp_id"
   end
 
   create_table "reviews", force: :cascade do |t|
